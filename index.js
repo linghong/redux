985d3a92a06a175781ebeb904fc3923cf3c3e444
linghong <linghong_chen@hotmail.com>
2017-10-31 08:49:54 -0400

    added callback route.

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.get('/auth/google', passport.authenticate('google', {
 	scope: ['profile', 'email']
 }));
 
+app.get('auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
