d1ac93b5f418de702b5c0e17a8473f749339ebe5
linghong <linghong_chen@hotmail.com>
2017-10-31 12:49:35 -0400

    created a model class for User.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
 require('./services/passport');
+require('./models/user');
+
 const authRoute = require('./routes/authRoutes');
 
 mongoose.connect(keys.mongoURI, "authSource=admin");
