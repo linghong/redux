cd03bee170daf25c2c69fca5832206845db542de
linghong <linghong_chen@hotmail.com>
2017-10-31 13:03:09 -0400

    added new user with googleId.

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose =require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
 	new GoogleStrategy({
@@ -8,6 +11,6 @@ passport.use(
 		clientSecret: keys.googleClientSecret,
 		callbackURL: '/auth/google/callback'
 	}, (accessToken, refreshToken, profile, done)=>{
-		console.log(accessToken, refreshToken, profile);
+		new User({googleId: profile.id}).save();
 	}
 ));
