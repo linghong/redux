69ddc27a2e40171ff6bfdff1b72c5e4124d0c8dd
linghong <linghong_chen@hotmail.com>
2017-10-31 12:35:46 -0400

    added mongoose and connected to a remote mongo databse.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 require('./services/passport');
 const authRoute = require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoURI, "authSource=admin");
 
 const app = express();
 authRoute(app);
