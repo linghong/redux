b194c5ca3b44eb0190301b15c42a86bd286fccb9
linghong <linghong_chen@hotmail.com>
2017-10-31 23:24:46 -0400

    fixed a typo.

--- services/passport.js
+++ services/passport.js
@@ -13,11 +13,16 @@ passport.use(
 		clientSecret: keys.googleClientSecret,
 		callbackURL: '/auth/google/callback'
 	}, (accessToken, refreshToken, profile, done)=>{
-		User.findOne({googleId: profilr.id})
+		User.findOne({googleId: profile.id})
 		.then((existingUser)=>{
 			if(!existingUser){
-				new User({googleId: profile.id}).save();
+				new User({googleId: profile.id})
+					.save()
+					.then(user=>done(null, user));
+			} else{
+				done(null, existingUser);
 			}
+			
 		});
 	}		
 ));
